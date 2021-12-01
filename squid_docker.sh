cc188a7d3a34a26e9973ad2291ff045c658ae29c
enescva <87376170+enescva@users.noreply.github.com>
2021-12-01 16:00:35 +0100

    Update squid_docker.sh

--- squid_docker.sh
+++ squid_docker.sh
@@ -1,2 +1,4 @@
 docker build -t squid-auth:1.0 .
-docker run --name squid -d --publish 3129:3128 -e PROXY_USERNAME=enescva -e PROXY_PASSWORD=efsa --volume /c/Squid/logs:/var/log/squid  squid-auth:1.0
+mkdir logs
+sudo chown proxy logs
+docker run --name squid -d --publish 3129:3128 -e PROXY_USERNAME=enescva -e PROXY_PASSWORD=efsa --volume /home/gitpod/workspace/squid-proxy-basic-auth/logs:/var/log/squid  squid-auth:1.0
