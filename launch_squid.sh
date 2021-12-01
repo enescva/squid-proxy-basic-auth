688a5f6286de8cef985e028109668a01ef267f95
enescva <valentin.enescu@efsa.europa.eu>
2021-12-01 18:48:18 +0000

    update

--- launch_squid.sh
+++ launch_squid.sh
@@ -1,4 +1,5 @@
 docker build -t squid-auth:1.0 .
-mkdir /workspace/logs
-sudo chown proxy /workspace/logs
-docker run --name squid -d --publish 3129:3128 -e PROXY_USERNAME=enescva -e PROXY_PASSWORD=efsa --volume /workspace/logs:/var/log/squid  squid-auth:1.0
+mkdir logs
+sudo chown proxy:proxy logs
+sudo chmod ugo+w logs
+docker run --name squid -d --publish 3129:3128 -e PROXY_DEBUG -e PROXY_USERNAME=enescva -e PROXY_PASSWORD=efsa --volume /workspace/squid-proxy-basic-auth/logs:/var/log/squid  squid-auth:1.0
